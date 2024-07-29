7d9e180e7933d6986b9e0d614e71c0358ab3d61e
William Yang <77467499+wilyan09007@users.noreply.github.com>
2024-07-28 19:05:47 -0700

    failing visualization of waypoints

--- competition_code/new_code/competition_runner.py
+++ competition_code/new_code/competition_runner.py
@@ -174,6 +174,41 @@ async def evaluate_solution(
     )
     rule = RoarCompetitionRule(waypoints,vehicle,world) # 3 laps
 
+    import transforms3d as tr3d
+
+    waypoint_list : List[roar_py_interface.RoarPyWaypoint] = []
+    trace_waypoint_laneid = 1
+    # print("LaneIDS: ", world.comprehensive_waypoints.keys())
+    for lane_id in [0, 1]:
+        lane_waypoints = world.comprehensive_waypoints[lane_id]
+        assert len(lane_waypoints) % 2 == 0
+        for i in range(len(lane_waypoints)//2):
+            first_waypoint = lane_waypoints[2*i]
+            second_waypoint = lane_waypoints[2*i+1]
+            real_waypoint = roar_py_interface.RoarPyWaypoint.from_line_representation(
+                first_waypoint.location,
+                second_waypoint.location,
+                first_waypoint.roll_pitch_yaw
+            )
+            # print(real_waypoint)
+            waypoint_list.append(real_waypoint)
+
+
+    # roar_py_instance.close()
+    # np.savez_compressed("Monza.npz", **roar_py_interface.RoarPyWaypoint.save_waypoint_list(waypoint_list))
+
+    for waypoint in waypoint_list:
+        origin_loc = waypoint.location
+        forward_loc = waypoint.location + tr3d.euler.euler2mat(*waypoint.roll_pitch_yaw) @ np.array([0.5,0.0,0.0])
+        world.carla_world.debug.draw_arrow(
+            roar_py_carla.location_to_carla(origin_loc),
+            roar_py_carla.location_to_carla(forward_loc),
+            thickness=0.1,
+            arrow_size=0.1,
+            color=carla.Color(0,255,0),
+            life_time=-1.0
+        )
+
     for _ in range(20):
         await world.step()
     
